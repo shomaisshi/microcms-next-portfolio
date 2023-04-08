459e43db5ae1aa646f4bcbb0f166f7ee186a0b33
shoma1219 <shoma.s.1219@gmail.com>
2023-04-09 02:21:35 +0900

    [update]

--- portfolio/components/Gamepad.jsx
+++ portfolio/components/Gamepad.jsx
@@ -11,6 +11,26 @@ export default function Gamepad() {
         canvas.height = 500;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        function make_actor(name, x, y, w, h, key) {
+            const a = {}
+            a.name = name;
+            a.x = x;
+            a.y = y;
+            a.w = w;
+            a.h = h;
+            a.key = key
+            return a;
+        }
+
+        const leftButton = make_actor('Left', 30, 100, 80, 60, 37);
+        const rightButton = make_actor('Right', 150, 100, 80, 60, 39);
+        const upButton = make_actor('Up', 100, 20, 60, 80, 38);
+        const downButton = make_actor('Down', 100, 160, 60, 80, 40);
+
+        const zButton = make_actor('Z', 300, 40, 60, 80, 90);
+        const xButton = make_actor('X', 260, 100, 60, 80, 88);
+
+
         let isButtonPressed = false;
 
         const drawButton = (x, y, width, height, text) => {
@@ -48,56 +68,69 @@ export default function Gamepad() {
             const y = event.clientY - rect.top;
 
             // left 
-            updateButton(x, y, 30, 100, 80, 60, 'left', 37)
+            updateButton(x, y, leftButton.x, leftButton.y, leftButton.w, leftButton.h, leftButton.name, leftButton.key)
             // right 
-            updateButton(x, y, 150, 100, 80, 60, 'right', 39)
+            updateButton(x, y, rightButton.x, rightButton.y, rightButton.w, rightButton.h, rightButton.name, rightButton.key)
             // up 
-            updateButton(x, y, 100, 20, 60, 80, 'up', 38)
+            updateButton(x, y, upButton.x, upButton.y, upButton.w, upButton.h, upButton.name, upButton.key)
             // down 
-            updateButton(x, y, 100, 160, 60, 80, 'down', 40)
+            updateButton(x, y, downButton.x, downButton.y, downButton.w, downButton.h, downButton.name, downButton.key)
 
             // z
-            updateButton(x, y, 300, 40, 60, 80, '弾発射', 90)
+            updateButton(x, y, zButton.x, zButton.y, zButton.w, zButton.h, zButton.name, zButton.key)
             // x
-            updateButton(x, y, 260, 100, 60, 80, 'ボックス', 88)
+            updateButton(x, y, xButton.x, xButton.y, xButton.w, xButton.h, xButton.name, xButton.key)
         };
 
+        function endButton(name, x, y, w, h, key) {
+            isButtonPressed = false;
+            drawButton(x, y, w, h, name);
+            document.dispatchEvent(new KeyboardEvent('keyup', { keyCode: key }));
+        }
         const handleMouseUp = () => {
-            // isButtonPressed = false;
-            // drawButton(50, 20, 100, 60, 'left');
-            // document.dispatchEvent(new KeyboardEvent('keyup', { keyCode: 37 }));
+            endButton('Left', 30, 100, 80, 60, 37);
+            endButton('Right', 150, 100, 80, 60, 39);
+            endButton('Up', 100, 20, 60, 80, 38);
+            endButton('Down', 100, 160, 60, 80, 40);
 
-            // isButtonPressed = false;
-            // drawButton(120, 20, 100, 60, 'right');
-            // document.dispatchEvent(new KeyboardEvent('keyup', { keyCode: 39 }));
+            endButton('Z', 300, 40, 60, 80, 90);
+            endButton('X', 260, 100, 60, 80, 88);
         };
+
 
         const handleTouchStart = (event) => {
             event.preventDefault();
             const rect = canvas.getBoundingClientRect();
-            const joisticx = event.touches[0].clientX - rect.left;
-            const joisticy = event.touches[0].clientY - rect.top;
 
-            // left 
-            updateButton(joisticx, joisticy, 30, 100, 80, 60, 'left', 37)
-            // right 
-            updateButton(joisticx, joisticy, 150, 100, 80, 60, 'right', 39)
-            // up 
-            updateButton(joisticx, joisticy, 100, 20, 60, 80, 'up', 38)
-            // down 
-            updateButton(joisticx, joisticy, 100, 160, 60, 80, 'down', 40)
+            for (var i = 0; i < event.touches.length; i++) {
+                const x = event.touches[i].clientX - rect.left;
+                const y = event.touches[i].clientY - rect.top;
 
-            const buttonx = event.touches[1].clientX - rect.left;
-            const buttony = event.touches[1].clientY - rect.top;
-            // z
-            updateButton(buttonx, buttony, 300, 40, 60, 80, '弾発射', 90)
-            // x
-            updateButton(buttonx, buttony, 260, 100, 60, 80, 'ボックス', 88)
+                // left 
+                updateButton(x, y, leftButton.x, leftButton.y, leftButton.w, leftButton.h, leftButton.name, leftButton.key)
+                // right 
+                updateButton(x, y, rightButton.x, rightButton.y, rightButton.w, rightButton.h, rightButton.name, rightButton.key)
+                // up 
+                updateButton(x, y, upButton.x, upButton.y, upButton.w, upButton.h, upButton.name, upButton.key)
+                // down 
+                updateButton(x, y, downButton.x, downButton.y, downButton.w, downButton.h, downButton.name, downButton.key)
+
+                // z
+                updateButton(x, y, zButton.x, zButton.y, zButton.w, zButton.h, zButton.name, zButton.key)
+                // x
+                updateButton(x, y, xButton.x, xButton.y, xButton.w, xButton.h, xButton.name, xButton.key)
+            }
+
         };
+
         const handleTouchEnd = () => {
-            isButtonPressed = false;
-            drawButton(50, 20, 100, 60, 'left');
-            document.dispatchEvent(new KeyboardEvent('keyup', { keyCode: 37 }));
+            endButton('Left', 30, 100, 80, 60, 37);
+            endButton('Right', 150, 100, 80, 60, 39);
+            endButton('Up', 100, 20, 60, 80, 38);
+            endButton('Down', 100, 160, 60, 80, 40);
+
+            endButton('Z', 300, 40, 60, 80, 90);
+            endButton('X', 260, 100, 60, 80, 88);
         };
 
         canvas.addEventListener('mousedown', handleMouseDown);
